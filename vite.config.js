c9e3e2f1b2e01280ee7357e4866659b3ca677292
21blackjac <chaiqmustapha@gmail.com>
2025-08-13 04:36:27 +0100

    vite.config.js changes for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/', // Explicit base URL (helps with asset paths)
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true, // Clears dist folder before rebuild
+    chunkSizeWarningLimit: 1600, // Avoids chunk size warnings
+  },
+  server: {
+    host: true // Needed for Netlify's live preview
+  }
 })
