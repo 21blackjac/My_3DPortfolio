9b85a3fddb83b0a817edea1c0ec0485daa99f236
21blackjac <chaiqmustapha@gmail.com>
2025-09-04 14:58:38 +0100

    Fixing the android issue

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,7 @@ const ServiceCard = ({ index, title, icon }) => (
           scale: 1,
           speed: 450,
         }}
-        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex! justify-evenly items-center flex-col"
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex-important justify-evenly items-center flex-col"
       >
         <img
           src={icon}
@@ -38,23 +38,26 @@ const About = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText} style={{fontSize:"22px"}}>Introduction</p>
+        <p className={styles.sectionSubText} style={{ fontSize: "22px" }}>
+          Introduction
+        </p>
         <h2 className={styles.sectionHeadText}>Overview</h2>
       </motion.div>
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 mb-4 text-secondary text-[22px] max-w-3xl leading-[30px]" style={{margin:"40px 0"}}
+        className="mt-4 mb-4 text-secondary text-[22px] max-w-3xl leading-[30px]"
+        style={{ margin: "40px 0" }}
       >
-        I'm a skilled full-stack developer with hands-on experience building modern web
-        applications using MongoDB, Express, React, Node.js, MySQL, and Laravel.
-        I've built secure admin dashboards, financial tools, and authentication
-        systems with JWT and Clerk (OAuth2). I also have a background in AI,
-        data science, and systems administration, and I'm always eager to
-        explore new tools and challenges.
+        I'm a skilled full-stack developer with hands-on experience building
+        modern web applications using MongoDB, Express, React, Node.js, MySQL,
+        and Laravel. I've built secure admin dashboards, financial tools, and
+        authentication systems with JWT and Clerk (OAuth2). I also have a
+        background in AI, data science, and systems administration, and I'm
+        always eager to explore new tools and challenges.
       </motion.p>
 
-      <div className="mt-20 mb-[20px] flex gap-10" style={{margin:"40px 0"}}>
+      <div className="mt-20 mb-[20px] flex gap-10" style={{ margin: "40px 0" }}>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
